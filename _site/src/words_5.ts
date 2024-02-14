4f4170e853acbb7684115415713febb58bce4a55
abby-albert <abigailalbert2007@gmail.com>
2024-02-14 10:34:24 -0800

    game

---
+++ _site/src/words_5.ts
@@ -0,0 +1,24 @@
+const words = {
+	"words": [
+		"burns",
+		"churns",
+		"acids",
+		"bases",
+		"brown",
+		"touch",
+		"flush",
+		"smell",
+		"sense",
+		"carbo",
+		"milli",
+		"centi",
+		"glass",
+		"flask",
+		"amine",
+		"oxide",
+		"thermo",
+		"hydro",
+		"chems",
+	]
+};
+export default words;
